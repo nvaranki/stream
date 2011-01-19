6fc3672494cf8e456832d30d36b8a85abc6d7780
Nikolai Varankine <nvaranki@tut.by>
2011-01-19 00:53:56 +0000

    minor clean-up and re-factoring

--- src/com/varankin/io/stream/CharacterReader.java
+++ src/com/varankin/io/stream/CharacterReader.java
@@ -6,23 +6,33 @@ import java.io.*;
  * {@link java.lang.Iterable} wrapper over character 
  * {@link java.io.Reader}.
  *
- * @author &copy; 2009 Nikolai Varankine
+ * @author &copy; 2011 Nikolai Varankine
  */
 public class CharacterReader extends AtomicReader<Character>
 {
-    public static Factory<Character> FACTORY = new Factory<Character>() 
-    {
-        @Override public Character newInstance( char aAtom )
-        {
-            return new Character( aAtom );
-        }
-    };
-    
     /**
      * @param aInput provider of characters.
+     * @param aUnique true to provide unique objects, false to supply shared copies.
      */
-    public CharacterReader( Reader aInput )
+    public CharacterReader( Reader aInput, boolean aUnique )
     {
-        super( aInput, FACTORY );
+        super( aInput, new CharacterFactory( aUnique ) );
+    }
+    
+    private static class CharacterFactory implements Factory<Character>
+    {
+        private final boolean unique;
+
+        private CharacterFactory( boolean aUnique )
+        {
+            unique = aUnique;
+        }
+
+        @Override
+        public Character newInstance( char aAtom )
+        {
+            // chars of Basic Multilingual Plane only
+            return unique ? new Character( aAtom ) : Character.valueOf( aAtom );
+        }
     }
 }
