79dba4ca7987f6f7b301c1ccc03ab36dcd67c26f
Nikolai Varankine <nvaranki@tut.by>
2011-04-25 12:11:21 +0000

    Iterator allowed at construction

--- src/com/varankin/io/stream/NullTerminator.java
+++ src/com/varankin/io/stream/NullTerminator.java
@@ -3,54 +3,66 @@ package com.varankin.io.stream;
 import java.util.*;
 
 /**
- * Passthrough iteratable object that ends iteration with extra null.
+ * Passthrough iteratable object that appends iteration with extra null.
  * 
  * @param T elementary object to transit.
  *
- * @author &copy; 2009 Nikolai Varankine
+ * @author &copy; 2011 Nikolai Varankine
  */
 public final class NullTerminator<T> implements Iterable<T>
 {
-    private final Iterator<T> source, factory;
+    private final Iterator<T> repeater;
 
     public NullTerminator( Iterable<T> aSource )
     {
-        source = aSource != null ? aSource.iterator() : Collections.<T>emptyList().iterator();
-        factory = new Iterator<T>()
-        {
-            private boolean send_null = true;
+        this( aSource != null ? aSource.iterator() : Collections.<T>emptyList().iterator() );
+    }
 
-            @Override public boolean hasNext()
-            {
-                return source.hasNext() || send_null;
-            }
-
-            @Override public T next()
-            {
-                if( source.hasNext() )
-                {
-                    return source.next();
-                }
-                else if( send_null )
-                {
-                    send_null = false;
-                    return null;
-                }
-                else
-                    throw new NoSuchElementException();
-            }
-
-            @Override public void remove()
-            {
-                source.remove();
-            }
-
-        };
+    public NullTerminator( Iterator<T> aSource )
+    {
+        repeater = new Repeater<T>( aSource != null ? aSource : Collections.<T>emptyList().iterator() );
     }
 
     @Override public Iterator<T> iterator()
     {
-        return factory;
+        return repeater;
+    }
+
+    private static class Repeater<T> implements Iterator<T>
+    {
+        private final Iterator<T> source;
+        private boolean send_null = true;
+
+        public Repeater( Iterator<T> aSource )
+        {
+            source = aSource;
+        }
+
+        @Override public boolean hasNext()
+        {
+            return source.hasNext() || send_null;
+        }
+
+        @Override public T next()
+        {
+            if( source.hasNext() )
+            {
+                return source.next();
+            }
+            else if( send_null )
+            {
+                send_null = false;
+                return null;
+            }
+            else
+                throw new NoSuchElementException();
+        }
+
+        @Override public void remove()
+        {
+            source.remove();
+        }
+
     }
 
 }
