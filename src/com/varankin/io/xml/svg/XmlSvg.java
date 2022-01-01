852bf32bedc98ec24f5025bd9d8b4901ebacb4ba
Nikolai Varankine <nvaranki@tut.by>
2022-01-01 18:08:26 +0300

    DB implementation code separated from "brains" project - dirty runnable

---
+++ src/com/varankin/io/xml/svg/XmlSvg.java
@@ -0,0 +1,52 @@
+package com.varankin.io.xml.svg;
+
+/**
+ *
+ * @author &copy; 2020 Николай Варанкин
+ */
+public interface XmlSvg 
+{
+    static final String XML_SVG = "svg";
+    
+    static final String SVG_ATTR_CLASS = "class";
+    static final String SVG_ATTR_HEIGHT = "height";
+    static final String SVG_ATTR_WIDTH = "width";
+    static final String SVG_ATTR_FONT_SIZE = "font-size";
+    static final String SVG_ATTR_X = "x";
+    static final String SVG_ATTR_Y = "y";
+    static final String SVG_ATTR_X1 = "x1";
+    static final String SVG_ATTR_Y1 = "y1";
+    static final String SVG_ATTR_X2 = "x2";
+    static final String SVG_ATTR_Y2 = "y2";
+    static final String SVG_ATTR_R = "r";
+    static final String SVG_ATTR_RX = "rx";
+    static final String SVG_ATTR_RY = "ry";
+    static final String SVG_ATTR_CX = "cx";
+    static final String SVG_ATTR_CY = "cy";
+    static final String SVG_ATTR_TRANSFORM = "transform";
+    static final String SVG_ATTR_POINTS = "points";
+    static final String SVG_ATTR_STROKE = "stroke";
+    static final String SVG_ATTR_FILL = "fill";
+    static final String SVG_ATTR_ID = "id";
+    
+    static final String SVG_TR_TRANSLATE = "translate";
+    static final String SVG_TR_ROTATE = "rotate";
+    static final String SVG_TR_SCALE = "scale";
+    static final String SVG_TR_SKEW_X = "skewX";
+    static final String SVG_TR_SKEW_Y = "skewY";
+    static final String SVG_TR_MATRIX = "matrix";
+    
+    static final String SVG_ELEMENT_A = "a";
+    static final String SVG_ELEMENT_G = "g";
+    static final String SVG_ELEMENT_CIRCLE = "circle";
+    static final String SVG_ELEMENT_ELLIPSE = "ellipse";
+    static final String SVG_ELEMENT_RECT = "rect";
+    static final String SVG_ELEMENT_LINE = "line";
+    static final String SVG_ELEMENT_POLYLINE = "polyline";
+    static final String SVG_ELEMENT_POLYGON = "polygon";
+    static final String SVG_ELEMENT_SYMBOL = "symbol";
+    static final String SVG_ELEMENT_TEXT = "text";
+    static final String SVG_ELEMENT_USE = "use";
+    
+    static final String XMLNS_SVG = "http://www.w3.org/2000/svg";
+}
